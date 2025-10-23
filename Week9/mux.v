4a41a0d97a03fa86a1e72007fde5eca96beda963
HudsonTran1 <hudsontran@my.cwi.edu>
2025-10-23 14:12:55 -0600

    Update mux

--- Week9/mux.v
+++ Week9/mux.v
@@ -1,5 +1,10 @@
 module mux(
-
+    input [15:0] in,
+    input [1:0] sel,
+    input enable,
+    output [3:0] out
 );
-
+    assign out = (sel == 0 ? out[3:0]:
+                 sel == 1 ? out[7:4]:
+                 sel == 2 ? out[11:8]: out[15:12]) & enable;
 endmodule
