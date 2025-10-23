7956dc023acca7baaffe3f49d282ed3b93c7d14b
watermelon2718 <ellyntran@u.boisestate.edu>
2025-10-23 14:13:37 -0600

    added demux and top

---
+++ Week9/top.v
@@ -0,0 +1,15 @@
+module top (
+    input [15:0] sw,
+    output [15:0] led
+);
+    wire muxOut;
+
+    mux mux_inst (
+        //assign stuff
+    );
+
+    demux demux_inst (
+        //assign stuff
+    );
+
+endmodule
