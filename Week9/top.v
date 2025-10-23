a7809afe294fec114c5c0162574e5dcff3ce5b57
HudsonTran1 <hudsontran@my.cwi.edu>
2025-10-23 14:21:17 -0600

    Top

--- Week9/top.v
+++ Week9/top.v
@@ -1,15 +1,10 @@
 module top (
     input [15:0] sw,
+    input btnL, btnU, btnD, btnR, btnC
     output [15:0] led
+
 );
-    wire muxOut;
-
-    mux mux_inst (
-        //assign stuff
-    );
-
-    demux demux_inst (
-        //assign stuff
-    );
+    mux mux_inst (sw, btnU, btnL, btnC, demux_inst.in);
+    demux demux_inst (mux_inst.out, btnU, btnL, btnC, led[3:0], led[7:4], led[11:8],led[15:12]);
 
 endmodule
