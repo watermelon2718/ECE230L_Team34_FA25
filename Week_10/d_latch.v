76a182fdbe9822d38ec659de571b407b50c70b20
watermelon2718 <ellyntran@u.boisestate.edu>
2025-10-30 13:42:54 -0600

    d latch updated

--- Week_10/d_latch.v
+++ Week_10/d_latch.v
@@ -4,6 +4,16 @@ module d_latch(
 );
 
     // Will contain D-Latch behavior
+    // S: (D NAND E) NAND ~Q
+    // R: (~D NAND E) NAND Q
+    // have to use reg and always block to mitigate 
+    //verilog issues???
+
+    always @(D, E) begin
+        if (E)
+            Q <= D;
+    end
+
+    assign NotQ = ~Q;
 
 endmodule
-
