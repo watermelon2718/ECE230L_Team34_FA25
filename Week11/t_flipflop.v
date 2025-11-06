cd38f61943e266e9db996b7a61fc126983b6ad49
watermelon2718 <ellyntran@my.cwi.edu>
2025-11-06 14:05:17 -0700

    Update t_flipflop.v

---
+++ Week11/t_flipflop.v
@@ -0,0 +1,17 @@
+// do I need a reset? 
+
+module t_flipflop (
+    input D, CLK, T,
+
+//should these be reg?
+    output reg Q,
+    output NotQ
+);
+
+//mux instance
+// takes T as enable switch, Q and NotQ as inputs
+
+//dflipflop instance
+
+
+endmodule
