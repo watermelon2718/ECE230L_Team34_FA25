feeef69d0429e73afa709bb60bbf19c25a7f3253
watermelon2718 <ellyntran@my.cwi.edu>
2025-11-13 11:27:45 -0700

    Created top.v and modulo divider files

---
+++ Week12/modulo_divider.v
@@ -0,0 +1,3 @@
+// we want to divide our 100 MHz clock
+//to get a clock that toggles less frequently lol
+// divide by 12 with a modulo divider
---
+++ Week12/top.v
@@ -0,0 +1,8 @@
+module top(
+    input btnU, btnC,
+    output [6:0] led
+);
+
+//ripple counter and modulo divider inst here
+
+endmodule
