93d0944fcab78b8cd27af938523d08c1849358d5
watermelon2718 <ellyntran@my.cwi.edu>
2025-11-20 12:16:22 -0700

    Implemented binary state machine

--- Week13/binary.v
+++ Week13/binary.v
@@ -1,1 +1,35 @@
-// Implement binary state machine
+module binary(
+    input w,
+    input clk,
+    output z
+);
+
+    wire [2:0] State;
+    wire [2:0] Next;
+
+    dff zero(
+        .D(Next[0]),
+        .clk(clk),
+        .Q(State[0])
+    );
+
+    dff one(
+        .D(Next[1]),
+        .clk(clk),
+        .Q(State[1])
+    );
+
+    dff two (
+        .D(Next[2]),
+        .clk(clk),
+        .Q(State[2])
+    );
+
+    //TODO
+    assign z = State[2] + State[1] & ~State[0];
+    assign Next[0] = ~w & ((~State[0] & ~State[1]) + (State[0] & State[1])) + (w & State[2]) & (~State[0] + ~State[1]); //probably wrong haha
+    assign Next[1] = ~State[1] & State[0] + State[1] & ~State[0] + ~State[1] & w & ~State[2];
+    assign Next[2] = w & (State[2] + State[1] & State [0]);
+
+endmodule
+
--- Week13/onehot.v
+++ Week13/onehot.v
@@ -1,1 +1,51 @@
-// Implement one-hot state machine
+module one_hot(
+    input w,
+    input clk,
+    output z
+);
+    wire Anext, Bnext, Cnext, Dnext, Enext;
+    wire Astate, Bstate, Cstate, Dstate, Estate;
+
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .Q(Astate)
+    );
+
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .Q(Bstate)
+    );
+
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .Q(Cstate)
+    );
+
+    dff Ddff(
+        .Default(1'b0),
+        .D(Dnext),
+        .clk(clk),
+        .Q(Dstate)
+    );
+
+    dff Edff (
+        .Default(1'b0),
+        .D(Enext),
+        .clk(clk),
+        .Q(Estate)
+    );
+
+    //TODO
+    assign z = Cstate;
+
+    assign Anext = ~w;
+    assign Bnext = w & Astate;
+    assign Cnext = (w & Bstate) | (w & Cstate);
+endmodule
+
