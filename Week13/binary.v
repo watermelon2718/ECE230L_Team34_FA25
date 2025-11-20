1c9da1cbefa406443964ba7302de38cb0b9d9f39
watermelon2718 <ellyntran@my.cwi.edu>
2025-11-20 13:48:46 -0700

    Update binary.v

--- Week13/binary.v
+++ Week13/binary.v
@@ -1,29 +1,32 @@
 module binary(
     input w,
-    input clk,
-    output [2:0] state, //is this right?
+    input clk, reset,
+    output [2:0] State, //is this right?
     output z
 );
 
-    wire [2:0] State;
+    // wire [2:0] State;
     wire [2:0] Next;
 
     dff zero(
         .D(Next[0]),
         .clk(clk),
-        .Q(State[0])
+        .Q(State[0]),
+        .reset(reset)
     );
 
     dff one(
         .D(Next[1]),
         .clk(clk),
-        .Q(State[1])
+        .Q(State[1]),
+        .reset(reset)
     );
 
     dff two (
         .D(Next[2]),
         .clk(clk),
-        .Q(State[2])
+        .Q(State[2]),
+        .reset(reset)
     );
 
     //TODO
@@ -31,7 +34,6 @@ module binary(
     assign Next[0] = ~w & ((~State[0] & ~State[1]) + (State[0] & State[1])) + (w & State[2]) & (~State[0] + ~State[1]); //probably wrong haha
     assign Next[1] = ~State[1] & State[0] + State[1] & ~State[0] + ~State[1] & w & ~State[2];
     assign Next[2] = w & (State[2] + State[1] & State [0]);
-    assign state = State; // help
 
 endmodule
 
